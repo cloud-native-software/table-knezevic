b0ad2264af792f6b03936c0b5be0526a911849b6
DESKTOP-PIKM64C\Win10 <knezevicmarko1995@gmail.com>
2023-12-12 23:02:43 +0100

    even and odd numbers

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const array = [23, 56, 3, 45, 78, 99];
+
+const array = [23, 56, 3, 45, 78, 99, 10];
 
 //==========> Sorting
 const sortedArray = array.sort((a, b) => {
-    return a - b;
-})
+  return a - b;
+});
 
-console.log("Sorted array: ", (sortedArray));
+console.log("Sorted array: ", sortedArray);
 
 //==========> Max and min
 var max = array[sortedArray.length - 1];
@@ -14,16 +15,31 @@ var min = array[0];
 console.log("The largest number is: ", max);
 console.log("The smallest number is: ", min);
 
+// we can do this much easier wit math library:
+//console.log("Max: ", Math.max(...array));
+//console.log("Min: ", Math.min(...array));
+
 //==========> Average
 var sum = 0;
-for(var i = 0; i < array.length; i++) {
-    sum += array[i];
+for (var i = 0; i < array.length; i++) {
+  sum += array[i];
 }
 var average = sum / array.length;
 
 console.log("The average number is: ", average);
 
-// we can do this much easier wit math library:
-//console.log("Max: ", Math.max(...array));
-//console.log("Min: ", Math.min(...array));
+//==========> Even and odd numbers in array
 
+const evenArray = [];
+const oddArray = [];
+
+array.forEach((number) => {
+  if (number % 2 === 0) {
+    evenArray.push(number);
+  } else {
+    oddArray.push(number);
+  }
+});
+
+console.log("Even numbers: ", evenArray.length);
+console.log("Odd numbers: ", oddArray.length);
